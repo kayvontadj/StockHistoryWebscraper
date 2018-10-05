4d4a962dc5b2b8422a1a0b66955b842bd04d4e88
unknown <kayvontadj@berkeley.edu>
2018-10-05 01:03:29 -0700

    CSV's are made with correct formatting. Got rid of column labels.

--- StockHistoryWebscraper.py
+++ StockHistoryWebscraper.py
@@ -25,47 +25,52 @@ tickers_longs = [sheet.cell_value(r,0) for r in range(len(sheet.col_values(0)))]
 path_shorts = path_closing_prices + "\\" + tickers_shorts[0] + ".csv"
 path_longs = path_closing_prices + "\\" + tickers_longs[0] + ".csv"
 
-historical_data = pdr.get_data_yahoo(tickers_shorts[0], start="2013-09-30", end="2018-09-30")
-historical_data_csv = historical_data.to_csv()
+historical_data_shorts = pdr.get_data_yahoo(tickers_shorts[0], start="2013-09-30", end="2018-09-30")
+historical_data_csv_shorts = historical_data_shorts.to_csv()
+historical_data_longs = pdr.get_data_yahoo(tickers_longs[0], start="2013-09-30", end="2018-09-30")
+historical_data_csv_longs = historical_data_longs.to_csv()
 
 csv_shorts = open(path_shorts, "w")
-csv_shorts.write(historical_data_csv)
+csv_shorts.write(historical_data_csv_shorts)
 csv_shorts.close()
 
 csv_longs = open(path_longs, "w")
-csv_longs.write(historical_data_csv)
+csv_longs.write(historical_data_csv_longs)
 csv_longs.close()
 
 
-data_date = pd.read_csv(path_shorts)["Date"].tolist()
+data_date_shorts = pd.read_csv(path_shorts)["Date"].tolist()
+data_date_longs = pd.read_csv(path_longs)["Date"].tolist()
 
 with open(path_closing_prices + "\\Temp_Shorts.csv", 'w', newline='') as f:
 	try:
 		thewriter = csv.writer(f, dialect='excel')
-		thewriter.writerow(["Date"] + data_date)
+		thewriter.writerow(["Date"] + data_date_shorts)
 		for ticker in tickers_shorts[0:2]:
-			print(3)
 			path = path_closing_prices + "\\" + ticker + ".csv"
 
 			historical_data = pdr.get_data_yahoo(ticker, start="2013-09-30", end="2018-09-30")
 			historical_data_csv = historical_data.to_csv()
+
 			csv_shorts = open(path, "w")
 			csv_shorts.write(historical_data_csv)
 			csv_shorts.close()
+
 			data_close = pd.read_csv(path)["Close"].tolist()
+
 			thewriter.writerow([ticker] + data_close)
-		temp = pd.read_csv(path_closing_prices + "\\Temp_Shorts.csv") #the buggy line
-		print(2)
-		temp_transpose = temp.transpose().to_csv()
-		csv_transpose = open(path_closing_prices + "\\Short Closing Prices.csv", "w")
-		csv_transpose.write(temp_transpose)
-		csv_transpose.close()
 	except ValueError:
 		pass
+temp = pd.read_csv(path_closing_prices + "\\Temp_Shorts.csv", index_col=0)
+temp_transpose = temp.transpose()
+temp_transpose=temp_transpose.to_csv()
+csv_transpose = open(path_closing_prices + "\\Short Closing Prices.csv", "w")
+csv_transpose.write(temp_transpose)
+csv_transpose.close()
 with open(path_closing_prices + "\\Temp_Longs.csv", 'w', newline='') as f:
-	thewriter = csv.writer(f, dialect='excel')
-	thewriter.writerow(["Date"] + data_date)
 	try:
+		thewriter = csv.writer(f, dialect='excel')
+		thewriter.writerow(["Date"] + data_date_longs)
 		for ticker in tickers_longs[0:2]:
 			path = path_closing_prices + "\\" + ticker + ".csv"
 
@@ -79,10 +84,10 @@ with open(path_closing_prices + "\\Temp_Longs.csv", 'w', newline='') as f:
 			data_close = pd.read_csv(path)["Close"].tolist()
 
 			thewriter.writerow([ticker] + data_close)
-		temp = pd.read_csv(path_closing_prices + "\\Temp_Longs.csv")
-		temp_transpose = temp.transpose().to_csv()
-		csv_transpose = open(path_closing_prices + "\\Long Closing Prices.csv", "w")
-		csv_transpose.write(temp_transpose)
-		csv_transpose.close()
 	except ValueError:
 		pass
+temp = pd.read_csv(path_closing_prices + "\\Temp_Longs.csv", index_col=0)
+temp_transpose = temp.transpose().to_csv()
+csv_transpose = open(path_closing_prices + "\\Long Closing Prices.csv", "w")
+csv_transpose.write(temp_transpose)
+csv_transpose.close()
