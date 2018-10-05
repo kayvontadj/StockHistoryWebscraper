e11a02ac6f4dae66cdb308f1d421ee54c4046ea2
unknown <kayvontadj@berkeley.edu>
2018-10-05 12:17:29 -0700

    Got rid of long/short difference.

--- StockHistoryWebscraper.py
+++ StockHistoryWebscraper.py
@@ -19,74 +19,38 @@ if not os.path.isdir(path_closing_prices):
 #get tickers
 workbook = xlrd.open_workbook("StockList.xlsx")
 sheet = workbook.sheet_by_index(0)
-tickers_shorts = [sheet.cell_value(r,2) for r in range(len(sheet.col_values(2)))]
-tickers_longs = [sheet.cell_value(r,0) for r in range(len(sheet.col_values(0)))]
+tickers = [sheet.cell_value(r,0) for r in range(len(sheet.col_values(0)))]
 
-path_shorts = path_closing_prices + "\\" + tickers_shorts[0] + ".csv"
-path_longs = path_closing_prices + "\\" + tickers_longs[0] + ".csv"
+ticker_path = path_closing_prices + "\\" + tickers[0] + ".csv"
 
-historical_data_shorts = pdr.get_data_yahoo(tickers_shorts[0], start="2013-09-30", end="2018-09-30")
-historical_data_csv_shorts = historical_data_shorts.to_csv()
-historical_data_longs = pdr.get_data_yahoo(tickers_longs[0], start="2013-09-30", end="2018-09-30")
-historical_data_csv_longs = historical_data_longs.to_csv()
+historical_data = pdr.get_data_yahoo(tickers[0], start="2013-09-30", end="2018-09-30").to_csv()
 
-csv_shorts = open(path_shorts, "w")
-csv_shorts.write(historical_data_csv_shorts)
-csv_shorts.close()
-
-csv_longs = open(path_longs, "w")
-csv_longs.write(historical_data_csv_longs)
-csv_longs.close()
+ticker_csv = open(ticker_path, "w")
+ticker_csv.write(historical_data)
+ticker_csv.close()
 
 
-data_date_shorts = pd.read_csv(path_shorts)["Date"].tolist()
-data_date_longs = pd.read_csv(path_longs)["Date"].tolist()
+dates = pd.read_csv(ticker_path)["Date"].tolist()
 
-with open(path_closing_prices + "\\Temp_Shorts.csv", 'w', newline='') as f:
+with open(path_closing_prices + "\\Temp.csv", 'w', newline='') as f:
 	thewriter = csv.writer(f, dialect='excel')
-	thewriter.writerow(["Date"] + data_date_shorts)
-	for ticker in tickers_shorts:
+	thewriter.writerow(["Date"] + dates)
+	for ticker in tickers:
 		try:
 			path = path_closing_prices + "\\" + ticker + ".csv"
 
-			historical_data = pdr.get_data_yahoo(ticker, start="2013-09-30", end="2018-09-30")
-			historical_data_csv = historical_data.to_csv()
+			historical_data = pdr.get_data_yahoo(ticker, start="2013-09-30", end="2018-09-30").to_csv()
+			csv = open(path, "w")
+			csv.write(historical_data)
+			csv.close()
 
-			csv_shorts = open(path, "w")
-			csv_shorts.write(historical_data_csv)
-			csv_shorts.close()
+			closing_prices = pd.read_csv(path)["Close"].tolist()
 
-			data_close = pd.read_csv(path)["Close"].tolist()
-
-			thewriter.writerow([ticker] + data_close)
+			thewriter.writerow([ticker] + closing_prices)
 		except ValueError:
 			pass
-temp = pd.read_csv(path_closing_prices + "\\Temp_Shorts.csv", index_col=0,error_bad_lines=False)
+temp = pd.read_csv(path_closing_prices + "\\Temp.csv", index_col=0,error_bad_lines=False)
 temp_transpose = temp.transpose().to_csv()
-csv_transpose = open(path_closing_prices + "\\Short Closing Prices.csv", "w")
-csv_transpose.write(temp_transpose)
-csv_transpose.close()
-with open(path_closing_prices + "\\Temp_Longs.csv", 'w', newline='') as f:
-	thewriter = csv.writer(f, dialect='excel')
-	thewriter.writerow(["Date"] + data_date_longs)
-	for ticker in tickers_longs:
-		try:
-			path = path_closing_prices + "\\" + ticker + ".csv"
-
-			historical_data = pdr.get_data_yahoo(ticker, start="2013-09-30", end="2018-09-30")
-			historical_data_csv = historical_data.to_csv()
-
-			csv_longs = open(path, "w")
-			csv_longs.write(historical_data_csv)
-			csv_longs.close()
-
-			data_close = pd.read_csv(path)["Close"].tolist()
-
-			thewriter.writerow([ticker] + data_close)
-		except ValueError:
-			pass
-temp = pd.read_csv(path_closing_prices + "\\Temp_Longs.csv", index_col=0, error_bad_lines=False)
-temp_transpose = temp.transpose().to_csv()
-csv_transpose = open(path_closing_prices + "\\Long Closing Prices.csv", "w")
+csv_transpose = open(path_closing_prices + "\\Closing Prices.csv", "w")
 csv_transpose.write(temp_transpose)
 csv_transpose.close()
