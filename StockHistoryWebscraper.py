f6c1eab3c9165a2c632cbe80c15c8c4e1159460c
unknown <kayvontadj@berkeley.edu>
2018-10-05 21:50:38 -0700

    Added user input, next step: think about GUI

--- StockHistoryWebscraper.py
+++ StockHistoryWebscraper.py
@@ -5,6 +5,7 @@ import pandas as pd
 import csv
 from pandas_datareader import data as pdr
 
+#gets ticker symbols from provided excel doc
 def getTickers(path, sheet_number=0, row=-1, column=-1):
 	workbook = xlrd.open_workbook(path)
 	sheet = workbook.sheet_by_index(sheet_number)
@@ -19,48 +20,59 @@ def getTickers(path, sheet_number=0, row=-1, column=-1):
 		tickers = [sheet.cell_value(row, c) for c in range(len(sheet.row_values(row)))]
 	return tickers
 
+#writes data file to csv
+def writetoCSV(path, data):
+	csv_file = open(path, "w")
+	csv_file.write(data)
+	csv_file.close()
+
 #fixes yahoo finance issues
 yf.pdr_override()
 
-path_closing_prices = os.environ["HOMEPATH"] + "\\Desktop\\Closing Prices"
+desired_value = input("Which value would you like to view: \nOpen	\nHigh	\nLow	\nClose	\nAdj Close \nVolume\n")
+possible_values = ["open", "high", "low", "close", "adjclose", "volume"]
+while desired_value.replace(' ', '').lower() not in possible_values:
+	print("Choose one of the given values\n")
+	desired_value = input("Which value would you like to view: \nOpen	\nHigh	\nLow	\nClose	\nAdj Close \nVolume\n")
+
+path_values = os.environ["HOMEPATH"] + "\\Desktop\\Stock Data" 
 
 #makes folder if one doesn't exist
-if not os.path.isdir(path_closing_prices):
-	os.makedirs(path_closing_prices)
-	print("Making 'Closing Prices' Folder in Desktop")
+if not os.path.isdir(path_values):
+	os.makedirs(path_values)
+	print("Making 'Stock Data' Folder in Desktop\n")
 
 tickers = getTickers("StockList.xlsx", column = 0)
 
-ticker_path = path_closing_prices + "\\" + tickers[0] + ".csv"
+ticker_path = path_values + "\\" + tickers[0] + ".csv"
 
 historical_data = pdr.get_data_yahoo(tickers[0], start="2013-09-30", end="2018-09-30").to_csv()
 
-ticker_csv = open(ticker_path, "w")
-ticker_csv.write(historical_data)
-ticker_csv.close()
+writetoCSV(ticker_path, historical_data)
+
 
 dates = pd.read_csv(ticker_path)["Date"].tolist()
 
-with open(path_closing_prices + "\\Temp.csv", 'w', newline='') as f:
+with open(path_values + "\\Temp.csv", 'w', newline='') as f:
 	thewriter = csv.writer(f, dialect='excel')
 	thewriter.writerow(["Date"] + dates)
 	for ticker in tickers[0:2]:
 		try:
-			path = path_closing_prices + "\\" + ticker + ".csv"
+			path = path_values + "\\" + ticker + ".csv"
 
 			historical_data = pdr.get_data_yahoo(ticker, start="2013-09-30", end="2018-09-30").to_csv()
 			csv = open(path, "w")
 			csv.write(historical_data)
 			csv.close()
 
-			closing_prices = pd.read_csv(path)["Close"].tolist()
+			values = pd.read_csv(path)[desired_value.title()].tolist()
 
-			thewriter.writerow([ticker] + closing_prices)
+			thewriter.writerow([ticker] + values)
 		except ValueError:
 			pass
-temp = pd.read_csv(path_closing_prices + "\\Temp.csv", index_col=0,error_bad_lines=False)
+temp = pd.read_csv(path_values + "\\Temp.csv", index_col=0,error_bad_lines=False)
 temp_transpose = temp.transpose().to_csv()
-os.remove(path_closing_prices + "\\Temp.csv")
-csv_transpose = open(path_closing_prices + "\\Closing Prices.csv", "w")
+os.remove(path_values + "\\Temp.csv")
+csv_transpose = open(path_values + "\\" + desired_value.title() + ".csv", "w")
 csv_transpose.write(temp_transpose)
 csv_transpose.close()
