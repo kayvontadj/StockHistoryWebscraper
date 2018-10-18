5470ba8b9d62587f2c30916c1c3d91874f0bb533
unknown <kayvontadj@berkeley.edu>
2018-10-17 23:08:21 -0700

    Added gui! Need to work on 'valid' dates

--- StockHistoryWebscraper.py
+++ StockHistoryWebscraper.py
@@ -4,7 +4,78 @@ import xlrd
 import pandas as pd
 import csv
 from pandas_datareader import data as pdr
+from tkinter import *
+import datetime as dt
 
+values = ["Open", "High", "Low", "Close", "Adj Close", "Volume"]
+desired_value = ""
+start_date = ""
+end_date = ""
+
+root = Tk()
+
+value = StringVar(root)
+value.set("--Select the type of information--")
+
+Label(root, text="Start Date").grid(row=0)
+Label(root, text="End Date").grid(row=1)
+start = Entry(root)
+end = Entry(root)
+start.grid(row=0,column=1)
+end.grid(row=1,column=1)
+
+def on_start_click(event):
+    """function that gets called whenever start is clicked"""
+    if start.get() == 'MM/DD/YYYY':
+       start.delete(0, "end") # delete all the text in the start
+       start.insert(0, '') #Insert blank for user input
+       start.config(fg = 'black')
+def on_end_click(event):
+    """function that gets called whenever start is clicked"""
+    if end.get() == 'MM/DD/YYYY':
+       end.delete(0, "end") # delete all the text in the start
+       end.insert(0, '') #Insert blank for user input
+       end.config(fg = 'black')
+def on_start_focusout(event):
+    if start.get() == '':
+        start.insert(0, 'MM/DD/YYYY')
+        start.config(fg = 'grey')
+def on_end_focusout(event):
+    if end.get() == '':
+        end.insert(0, 'MM/DD/YYYY')
+        end.config(fg = 'grey')
+
+
+start.insert(0, 'MM/DD/YYYY')
+start.bind('<FocusIn>', on_start_click)
+start.bind('<FocusOut>', on_start_focusout)
+start.config(fg = 'grey')
+
+end.insert(0, 'MM/DD/YYYY')
+end.bind('<FocusIn>', on_end_click)
+end.bind('<FocusOut>', on_end_focusout)
+end.config(fg = 'grey')
+
+value_menu = OptionMenu(root, value, *values)
+value_menu.grid(row=2,column=1)
+
+def getInputs():
+	global start_date, end_date, desired_value
+	desired_value = value.get()
+	start_date = start.get()
+	end_date = end.get()
+	start_date = dt.datetime.strptime(start_date, "%m/%d/%Y").isoformat(sep='T', timespec='auto')[0:10]
+	end_date = dt.datetime.strptime(end_date, "%m/%d/%Y").isoformat(sep='T', timespec='auto')[0:10]
+	#start_date = start_date[6:10] + '-' + start_date[0:2] + '-' + start_date[3:5]
+	#end_date = end_date[6:10] + '-' + end_date[0:2] + '-' + end_date[3:5]
+	print("Chosen value is", desired_value)
+	print("Chosen start date is", start_date)
+	print("Chosen end date is", end_date)
+	root.quit()
+button = Button(root, text="Run", command=getInputs)
+button.grid(row=3,column=1)
+mainloop()
+root.quit()
 #gets ticker symbols from provided excel doc
 def getTickers(path, sheet_number=0, row=-1, column=-1):
 	workbook = xlrd.open_workbook(path)
@@ -28,17 +99,6 @@ def writetoCSV(path, data):
 
 #fixes yahoo finance issues
 yf.pdr_override()
-
-desired_value = input("Which value would you like to view: \nOpen	\nHigh	\nLow	\nClose	\nAdj Close \nVolume\n")
-desired_value = desired_value.replace(' ','').title()
-possible_values = ["open", "high", "low", "close", "adjclose", "volume"]
-
-while desired_value.replace(' ', '').lower() not in possible_values:
-	print("Choose one of the given values\n")
-	desired_value = input("Which value would you like to view: \nOpen	\nHigh	\nLow	\nClose	\nAdj Close \nVolume\n")
-
-start_date = input("Choose a starting date in the form YYYY-MM-DD\n")
-end_date = input("Choose an end date in the form YYYY-MM-DD\n")
 
 path_values = os.environ["HOMEPATH"] + "\\Desktop\\Stock Data" 
 
